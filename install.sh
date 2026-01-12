2c47d474c9e78c6d8666a67133cbacf43bfc9179
Carter Prince <carteraprince@gmail.com>
2026-01-12 10:55:39 -0500

    make install.sh more robust, add gitignore

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -ex
 
+PREFIX="/usr/local"
+
 go mod tidy
-go build
-sudo install ./newseum /usr/local/bin/newseum
-sudo install ./newseum.desktop /usr/share/applications/newseum.desktop
+go build -ldflags "-s -w" -o newseum
+
+[ -d ${PREFIX}/bin ] || sudo mkdir -p ${PREFIX}/bin
+sudo install ./newseum ${PREFIX}/bin/newseum
+[ -d ${PREFIX}/share/applications ] || sudo mkdir -p ${PREFIX}/share/applications
+sudo install -m 644 ./newseum.desktop ${PREFIX}/share/applications/newseum.desktop
